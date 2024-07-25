5d2404c01bab2494c9dadb89bccda4c19613be50
Andrei Riaskov <code@riaskov.com>
2024-07-25 07:56:57 +0300

    feat: add app.module.ts

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from "@nestjs/common"
+import { ConfigModule } from "@nestjs/config"
+import * as Joi from "joi"
+import { StorageModule } from "./storage/storage.module"
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        HOST: Joi.string().default("localhost"),
+        PORT: Joi.number().default(3032),
+        POSTGRES_HOST: Joi.string().default("localhost"),
+        POSTGRES_PORT: Joi.number().default(5432),
+        POSTGRES_USER: Joi.string().default("one_time_link"),
+        POSTGRES_PASSWORD: Joi.string().allow("").optional(),
+        POSTGRES_DB: Joi.string().default("one_time_link"),
+      }),
+      isGlobal: true,
+    }),
+    StorageModule,
+  ],
+})
+export class AppModule {}
