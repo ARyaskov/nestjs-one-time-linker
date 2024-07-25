fcd3206e1786d5870d2e959236bc11affbb0bfbc
Andrei Riaskov <code@riaskov.com>
2024-07-26 01:22:30 +0300

    devops: enable pgcrypto extension in init-db script

--- init-db/init-db.sh
+++ init-db/init-db.sh
@@ -20,6 +20,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$POSTGRES_DB')\gexec
 
   GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;
+
+  CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 EOSQL
 
 echo "User and database creation script completed."
